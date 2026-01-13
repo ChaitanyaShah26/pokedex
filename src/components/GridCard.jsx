357a50d2f16a5699639075b2fb5fe35737577fcf
Chaitanya Shah <sendittochaitanya@gmail.com>
2026-01-13 22:40:50 +0530

    Add types in card

--- src/components/GridCard.jsx
+++ src/components/GridCard.jsx
@@ -12,6 +12,10 @@ function GridCard({name, idNum})
             <h4>#{PokeId}</h4>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZz7bEAtF3hDL0MgUoTWQ1FMC6OisPj2MK9A&s"></img>
             <p>{PokemonName}</p>
+            <div className='types'>
+                <span>Type 1</span>
+                <span>Type 2</span>
+            </div>
         </div>
     </>
     );
